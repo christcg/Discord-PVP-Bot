4ff024fb591b4cb44ceef37e811e23593e09cc62
Christina Guan <christcg@usc.edu>
2019-01-02 15:21:40 -0800

    just a try

---
+++ bot.js
@@ -0,0 +1,43 @@
+//Initial configuration and bot set up from 
+//DigitalTrends and Renemari Padillo @ Medium
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Bot listening to message that starts with '!'
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !test
+            case 'test':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Success!'
+                });
+            break;
+         }
+     }
+});
